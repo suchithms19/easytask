dc6af20b619b8c441179395e9a1a65d05477b340
suchithms19 <mssuchith2@gmail.com>
2025-09-11 14:20:37 +0530

    added name to user

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -7,5 +10,5 @@ import { Component } from '@angular/core';
   styleUrl: './user.css'
 })
 export class User {
-
+  selectedUser = DUMMY_USERS[randomIndex];
 }
