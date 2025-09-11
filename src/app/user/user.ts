d35a804ff5a3f041d47f8e04420552351fefeb3c
suchithms19 <mssuchith2@gmail.com>
2025-09-11 14:48:10 +0530

    add getters to user component

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -11,4 +11,12 @@ const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
 })
 export class User {
   selectedUser = DUMMY_USERS[randomIndex];
+
+  get imagePath() {
+    return "assets/users/" + this.selectedUser.avatar ;
+  }
+
+  get imageAlt() {
+    return this.selectedUser.name + "photo";
+  }
 }
