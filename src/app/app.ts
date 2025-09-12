0c178f81f228467ac5822a08eb3b2dc9e16ed4ee
suchithms19 <mssuchith2@gmail.com>
2025-09-12 11:43:32 +0530

    feat: working on i/o

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,17 +2,24 @@ import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
 import { User } from "./user/user";
 import { DUMMY_USERS } from './dummy-users';
+import { Tasks } from './tasks/tasks';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, User],
+  imports: [Header, User,Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
     users=DUMMY_USERS;
+    selectedUser='u1';
+
+    get selectedUserName(){
+        const user=this.users.find(u=>u.id===this.selectedUser);
+        return user?user.name:'';
+    }
 
     onSelect(id:string) {
-        console.log("selected user id:", id);
-    }
+      this.selectedUser=id;
+      } 
 }
