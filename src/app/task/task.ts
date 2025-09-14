3a99d546039ea06fb52d76b9e7bc2cc82982c13c
suchithms19 <mssuchith2@gmail.com>
2025-09-14 19:11:13 +0530

    feat: format due date in task component using Angular date pipe

--- src/app/task/task.ts
+++ src/app/task/task.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { type TaskData } from './task.type';
 import { Card } from '../ui/card/card';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task',
-  imports: [Card],
+  imports: [Card, DatePipe],
   templateUrl: './task.html',
   styleUrl: './task.css'
 })
