cd781a3b865eb46fe75c327521ff976586479e28
suchithms19 <mssuchith2@gmail.com>
2025-09-14 15:24:14 +0530

    feat: bind form inputs to component properties in add-task component

--- src/app/add-task/add-task.ts
+++ src/app/add-task/add-task.ts
@@ -1,13 +1,17 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-task.html',
   styleUrl: './add-task.css'
 })
 export class AddTask {
   @Output() onClick = new EventEmitter<void>();
+  enteredTitle ="";
+  enteredSummary="";
+  enteredDate="";
 
   onClickCancel(){
     this.onClick.emit();
