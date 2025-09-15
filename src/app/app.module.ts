3b88b0ba41713706709ae895acffd3a345990fa8
suchithms19 <mssuchith2@gmail.com>
2025-09-15 15:46:13 +0530

    feat: add SharedModule for card component and update app module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,15 @@ import { Header } from "./header/header";
 import { User } from "./user/user";
 import { Tasks } from "./tasks/tasks";
 import { BrowserModule } from "@angular/platform-browser";
-import { Card } from "./ui/card/card";
 import { Task } from "./task/task";
 import { AddTask } from "./add-task/add-task";
 import { FormsModule } from "@angular/forms";
+import { SharedModule } from "./ui/card/shared.module";
 
 @NgModule({
-    declarations: [App, Header, User, Card, Tasks, Task, AddTask],
+    declarations: [App, Header, User, Tasks, Task, AddTask],
     bootstrap: [App],
-    imports: [BrowserModule, FormsModule]
+    imports: [BrowserModule, FormsModule, SharedModule]
 })
 
 export class AppModule { }
